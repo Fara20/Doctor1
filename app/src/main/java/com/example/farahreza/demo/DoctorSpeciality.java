bad4266c7f80e25d1a636f1c193d9d3c38e85267
Anika Tahsin Raisa <41986485+Raisa2773@users.noreply.github.com>
2018-09-03 20:17:22 +0600

    Add files via upload

--- app/src/main/java/com/example/farahreza/demo/DoctorSpeciality.java
+++ app/src/main/java/com/example/farahreza/demo/DoctorSpeciality.java
@@ -12,7 +12,7 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.List;
 
-public class BookAppointment extends AppCompatActivity {
+public class DoctorSpeciality extends AppCompatActivity {
 
     Button BookConfirm;
     Spinner spinner;
@@ -32,15 +32,16 @@ public class BookAppointment extends AppCompatActivity {
         spinner = (Spinner) findViewById(R.id.spinner2);
         List<String> list = new ArrayList<String>();
         list.add("Select an Option");
-        list.add("Dhaka");
-        list.add("Chittagong");
-        list.add("Rajshahi");
-        list.add("Sylhet");
-        list.add("Commilla");
-        list.add("Barishal");
-        list.add("Khulna");
-        list.add("Mymensingh");
-        list.add("Rangpur");
+        list.add("Cardiology");
+        list.add("Neurology");
+        list.add("Surgery");
+        list.add("Gynaecology");
+        list.add("Pathology");
+        list.add("Neurosurgery");
+        list.add("Pediatrics");
+        list.add("Psychiatry");
+        list.add("Nephrology");
+        list.add("Dental");
 
 
         ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,R.layout.my_spinner, list);
@@ -52,7 +53,7 @@ public class BookAppointment extends AppCompatActivity {
         spinner = (Spinner) findViewById(R.id.spinner2);
         BookConfirm = (Button) findViewById(R.id.btnSubmit);
 
-        final Intent g =new Intent(getApplicationContext(),BookDocArea.class);
+        final Intent g =new Intent(getApplicationContext(),ChooseDoctor.class);
 
 
 
@@ -61,23 +62,25 @@ public class BookAppointment extends AppCompatActivity {
         BookConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(String.valueOf(spinner.getSelectedItem())=="Dhaka")
+                if(String.valueOf(spinner.getSelectedItem())=="Cardiology")
                     startActivity(g);
-                else  if(String.valueOf(spinner.getSelectedItem())=="Chittagong")
+                else   if(String.valueOf(spinner.getSelectedItem())=="Neurology")
                     startActivity(g);
-                else   if(String.valueOf(spinner.getSelectedItem())=="Comilla")
+                else   if(String.valueOf(spinner.getSelectedItem())=="Surgery")
                     startActivity(g);
-                else   if(String.valueOf(spinner.getSelectedItem())=="Rangpur")
+                else   if(String.valueOf(spinner.getSelectedItem())=="Gynaecology")
                     startActivity(g);
-                else   if(String.valueOf(spinner.getSelectedItem())=="Rajshahi")
+                else   if(String.valueOf(spinner.getSelectedItem())=="Pathology")
                     startActivity(g);
-                else   if(String.valueOf(spinner.getSelectedItem())=="Sylhet")
+                else   if(String.valueOf(spinner.getSelectedItem())=="Neurosurgery")
                     startActivity(g);
-                else   if(String.valueOf(spinner.getSelectedItem())=="Mymensingh")
+                else   if(String.valueOf(spinner.getSelectedItem())=="Pediatrics")
                     startActivity(g);
-                else   if(String.valueOf(spinner.getSelectedItem())=="Barishal")
+                else   if(String.valueOf(spinner.getSelectedItem())=="Psychiatry")
                     startActivity(g);
-                else   if(String.valueOf(spinner.getSelectedItem())=="Khulna")
+                else   if(String.valueOf(spinner.getSelectedItem())=="Nephrology")
+                    startActivity(g);
+                else   if(String.valueOf(spinner.getSelectedItem())=="Dental")
                     startActivity(g);
 
                 else
