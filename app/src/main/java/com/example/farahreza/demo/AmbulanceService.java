3b8f91418bf3e14fb24fade3acc598a7f009e924
zakir7213 <41986232+zakir7213@users.noreply.github.com>
2018-10-06 04:25:21 +0600

    Add files via upload

--- app/src/main/java/com/example/farahreza/demo/AmbulanceService.java
+++ app/src/main/java/com/example/farahreza/demo/AmbulanceService.java
@@ -8,6 +8,7 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -17,7 +18,7 @@ import com.google.firebase.database.ValueEventListener;
 
 public class AmbulanceService extends AppCompatActivity {
 EditText vecname,contact,email;
-    ClinicSignUpInformation user;
+
   //  Ambulance user;
     FirebaseAuth mAuth;
     DatabaseReference dRef, dRef1;
@@ -37,35 +38,15 @@ EditText vecname,contact,email;
         email=findViewById(R.id.email);
 
         mAuth=FirebaseAuth.getInstance();
-        dRef= FirebaseDatabase.getInstance().getReference("DoctorInfo");
-        dRef1=FirebaseDatabase.getInstance().getReference("ClinicSignUpInformation");
+        dRef= FirebaseDatabase.getInstance().getReference("Ambulance");
+
         session=new Session(this);
 
 
 
 
 
-        dRef1= FirebaseDatabase.getInstance().getReference().child("ClinicSignUpInformation");
-        usrqry=dRef1.orderByKey().equalTo(session.getusename());
 
-        usrqry.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for(DataSnapshot value:dataSnapshot.getChildren())
-                {
-                    user=value.getValue(ClinicSignUpInformation.class);
-                }
-
-
-                clinicname=user.getName();
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-                Toast.makeText(getApplicationContext(),"Error", Toast.LENGTH_LONG).show();
-            }
-        });
 
 
         btn.setOnClickListener(new View.OnClickListener() {
@@ -75,13 +56,16 @@ EditText vecname,contact,email;
                 strname=vecname.getText().toString().trim();
                 strcontact=contact.getText().toString().trim();
                 stremail=email.getText().toString().trim();
+                type="24 hours";
 
+                FirebaseUser user=mAuth.getCurrentUser();
+                String userid=user.getUid();
 
 
 
 
                 Ambulance newuser=new Ambulance(strname,strcontact,stremail,type);
-                dRef.child(clinicname).push().setValue(newuser);
+                dRef.child(userid).setValue(newuser);
 
                 Toast.makeText(getApplicationContext(), "Ambulance Inserted successfully!", Toast.LENGTH_SHORT).show();
             }
